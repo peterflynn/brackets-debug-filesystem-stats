e4b5e356b7e06f2a5d1018ea1b32104b0679b348
Peter Flynn <pflynn@adobe.com>
2014-02-20 14:58:06 -0800

    Initial commit of FS Stats debug extension

---
+++ README.md
@@ -0,0 +1,10 @@
+This extension helps gather info on FileSystem memory usage.
+
+## To gather FileSystem stats
+
+1. Use Brackets normally for a while
+2. Debug > Report FS Stats
+3. Debug > Show Developer Tools
+4. In the Console tab, copy everything between the "============" dividers
+5. In the Profiles tab, select Take Heap Snapshot and click the button to take a snapshot
+6. Record the total heap size (shown in the left-hand sidebar under "Snapshot 1"
---
+++ main.js
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2014 Peter Flynn.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define, brackets, $ */
+
+define(function (require, exports, module) {
+    "use strict";
+    
+    // Brackets modules
+    var CommandManager      = brackets.getModule("command/CommandManager"),
+        Menus               = brackets.getModule("command/Menus"),
+        DocumentManager     = brackets.getModule("document/DocumentManager"),
+        ProjectManager      = brackets.getModule("project/ProjectManager"),
+        FileSystem          = brackets.getModule("filesystem/FileSystem");
+    
+    function runReport() {
+        console.log("============ FileSystem object stats ============");
+
+        var sampleDir = ProjectManager.getProjectRoot();
+
+        /* @type {FileSystem} instance */
+        var fs = sampleDir._fileSystem;
+
+        /* @type {FileIndex} */
+        var index = fs._index;
+
+
+        var indexSize = Object.keys(index._index).length;
+
+        var numCachedFiles = 0,
+            numCachedDirs = 0,
+            numCachedStats = 0,
+            fileCacheBytes = 0,
+            nodeModulesFiles = 0;
+        
+        index.visitAll(function (entry, path) {
+            if (entry._stat) {
+                numCachedStats++;
+            }
+            if (entry.isFile) {
+                if (entry._contents !== null) {
+                    numCachedFiles++;
+                    fileCacheBytes += entry._contents.length;
+                    
+                    if (entry.fullPath.indexOf(".png") !== -1) {
+                        console.warn("Suspicious cached File " + entry);
+                    }
+                }
+                if (entry.fullPath.indexOf("node_modules") !== -1) {
+                    nodeModulesFiles++;
+                }
+            } else {
+                if (entry._contents) {
+                    numCachedDirs++;
+                }
+            }
+            if (entry.fullPath.indexOf(".git") !== -1) {
+                console.warn("Suspicious FS entry " + entry);
+            }
+        });
+
+        var nDocumentsAlive = DocumentManager.getAllOpenDocuments().length;
+
+        console.log("Files in index:", indexSize);
+        console.log("Files under node_modules:", nodeModulesFiles);
+        console.log("Files with content cached:", numCachedFiles);
+        console.log("Total File cached content (B):", fileCacheBytes);
+        console.log("Dirs with content cached:", numCachedDirs);
+        console.log("Documents kept alive:", nDocumentsAlive);
+        console.log("=================================================");
+    }
+    
+    
+    // Register command
+    var COMMAND_ID = "pflynn.debug.filesystem.stats";
+    CommandManager.register("Report FS Stats", COMMAND_ID, runReport);
+    
+    var menu = Menus.getMenu("debug-menu");
+    menu.addMenuItem(COMMAND_ID, null);
+});
